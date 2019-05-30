1df063406389bc50b1234e117f5ff4440e6ceb67
こけ <koni.ken.engineer@gmail.com>
2019-05-30 19:56:56 +0900

    implement function log-in

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -1,4 +1,6 @@
 class CopiesController < ApplicationController
+  before_action :copy_params,only:[:create]
+
   def index
     @copies = Copy.order("created_at DESC")
   end
@@ -8,15 +10,21 @@ class CopiesController < ApplicationController
   end
 
   def create
-    @copy = Copy.create(catchphrase: copy_params[:catchphrase],odai_id: copy_params[:odai_id])
+    
+    Copy.create(catchphrase: copy_params[:catchphrase], image: copy_params[:image], presentation: copy_params[:presentation], odai_id: params[:odai_id], user_id: current_user.id)
+    # binding.pry
+    # @copy = Copy.new(copy_params)
     redirect_to root_path
+
   end
 
   private
 
   def copy_params
     # binding.pry
-    params.require(:copy).permit(:catchphrase,:odai_id)
+    # params.permit(:catchphrase,:image,:presentation)
+    params.require(:copy).permit(:catchphrase,:image,:presentation,:odai_id)
+    # binding.pry
   end
 
 end
