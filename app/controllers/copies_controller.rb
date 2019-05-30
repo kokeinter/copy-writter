5932a0f096c5f9928e7ba075384d62f59b558df6
こけ <koni.ken.engineer@gmail.com>
2019-05-30 14:49:19 +0900

    edit action

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -4,5 +4,19 @@ class CopiesController < ApplicationController
   end
 
   def new
+    @copy = Copy.new
   end
+
+  def create
+    @copy = Copy.create(catchphrase: copy_params[:catchphrase],odai_id: copy_params[:odai_id])
+    redirect_to root_path
+  end
+
+  private
+
+  def copy_params
+    # binding.pry
+    params.require(:copy).permit(:catchphrase,:odai_id)
+  end
+
 end
--- app/controllers/odais_controller.rb
+++ app/controllers/odais_controller.rb
@@ -1,2 +1,12 @@
 class OdaisController < ApplicationController
+
+  def index
+    @odais = Odai.order("created_at DESC")
+  end
+
+  def new
+    @odai = Odai.new
+  end
+
+
 end
