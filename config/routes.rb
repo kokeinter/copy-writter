59275656ec969fedd2763a9b0e5ecf38eac19616
こけ <koni.ken.engineer@gmail.com>
2019-06-04 15:35:21 +0900

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'copies#index'
   resources :users
-  resources :odais do
+  resources :odais , only: [:index, :new, :create, :edit, :update, :destroy]do
     resources :copies do
-      resources :comments
+      resources :comments , only: [:create, :destroy]
     end
   end
+  resources :likes, only: [:create, :destroy]
 end
