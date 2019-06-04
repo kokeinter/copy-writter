9ee3a4fe387bb483c472f9875d418d62368eb831
こけ <koni.ken.engineer@gmail.com>
2019-06-04 20:48:36 +0900

    Like function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+
+  def create
+    Like.create(copy_id: params[:copy_id], user_id: current_user.id)
+    redirect_to root_path
+  end
+
+  def destroy
+    # binding.pry
+    like = Like.find(params[:id])
+    like.destroy if like.user_id == current_user.id
+    redirect_to root_path
+  end
+
+  private
+
+  def like_params
+    params.permit(:copy_id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :odais , only: [:index, :new, :create, :edit, :update, :destroy]do
     resources :copies do
       resources :comments , only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
     end
   end
-  resources :likes, only: [:create, :destroy]
 end
