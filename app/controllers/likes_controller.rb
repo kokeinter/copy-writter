110287378a16ff2748dc16771b2b523524af7636
こけ <koni.ken.engineer@gmail.com>
2019-06-06 20:34:52 +0900

    modify details

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,10 +6,24 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    # binding.pry
+
+    # # binding.pry
     like = Like.find(params[:id])
     like.destroy if like.user_id == current_user.id
     redirect_back(fallback_location: root_path)
+
+    # ajax実装時に使う
+    # @like = Like.find_by(params[:id])
+    # copy_id = @like.copy_id
+    # @copy = Copy.find_by(id: copy_id)
+    # odai_id = @copy.odai_id
+    # @odai = Odai.find_by(id: odai_id)
+    # @like.destroy if @like.user_id == current_user.id
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
+
   end
 
   private
